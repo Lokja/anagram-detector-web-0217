ecc4b9c27322a002b40369b696850b00f5d702e2
Devin Rieger <dev0089@gmail.com>
2017-02-08 16:22:11 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.find_all {|word| word.split("").sort == @word.split("").sort}
+  end
+
+end
+
+test1 = Anagram.new("abc")
+array = ["abc","aab","cba"]
+test1.match(array)
